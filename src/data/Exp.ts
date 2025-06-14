8b7d904a7ab75c678de04966736a1ffbfb8dd6ff
Your Name <you@example.com>
2025-06-14 00:15:18 -0500

    Modificacion archivos ts

--- src/data/Exp.ts
+++ src/data/Exp.ts
@@ -6,7 +6,7 @@ export const expMock = [
     title: "SISCOMPUTO",
     details: "Analista de Sistemas",
     description:
-      "Profesional con más de dos años de experiencia en desarrollo de software, especializado en IBM Domino / HCL Domino, con enfoque en front-end y conocimientos en back-end. para optimización de interfaces y funcionalidades interactivas. He trabajado en el desarrollo y parametrización de módulos para sistemas de gestión documental, adaptándolos a las necesidades de diferentes áreas empresariales.",
+      "Profesional con más de dos años de experiencia en desarrollo de software, especializado en IBM Domino / HCL Domino, con enfoque en front-end y conocimientos en back-end. para optimización de interfaces y funcionalidades interactivas. He trabajado en el desarrollo y parametrización de módulos para sistemas de gestión documental, adaptándolos a las necesidades de diferentes áreas empresarial.",
   },
    {
     id: "fasmee",
